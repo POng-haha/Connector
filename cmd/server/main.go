d8038e907fd3f7086dba894240ac37a9d6d6b88a
POng-haha <uroboros54150@gmail.com>
2025-08-21 21:06:51 +0700

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,80 +1,119 @@
-package api
+package handler
 
 import (
-	"bytes"
-	"context"
-	"encoding/xml"
-	"fmt"
-	"io"
 	"net/http"
-	"net/url"
-	"sync"
+	
+	"connectorapi-go/internal/adapter/utils"
+	"connectorapi-go/internal/core/domain"
+	app_error "connectorapi-go/pkg/error"
 
-	"picoapi-go/pkg/config"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 
 	"go.uber.org/zap"
 )
 
-type HTTPClient struct {
-	mu     sync.RWMutex
-	client map[string]*http.Client
-	logger *zap.SugaredLogger
-	config *config.Config
+// CollectionService defines the interface
+type CollectionService interface {
+	CollectionDetail(c *gin.Context, reqData domain.CollectionDetailRequest) (*domain.CollectionDetailResponse, *app_error.AppError)
+	CollectionLog(c *gin.Context, reqData domain.CollectionLogRequest) (*domain.CollectionLogResponse, *app_error.AppError)
 }
 
-func NewHTTPClient(cfg *config.Config, logger *zap.SugaredLogger) *HTTPClient {
-	client := make(map[string]*http.Client)
-	for name, certCfg := range cfg.Certs {
-		clientWithCerts, err := CreateHttpClientWithCert(certCfg.CACertPath)
-		if err != nil {
-			logger.Errorw("failed to create http client for cert", "cert", name, "error", err)
-			continue
-		}
-		client[name] = clientWithCerts
-	}
+// CollectionHandler handles all Collection-related API requests
+type CollectionHandler struct {
+	service CollectionService
+	validator *validator.Validate
+	logger  *zap.SugaredLogger
+	apikey  *utils.APIKeyRepository
+}
 
-	return &HTTPClient{
-		client: client,
-		logger: logger,
-		config: cfg,
+// NewCollectionHandler creates a new instance of CollectionHandler
+func NewCollectionHandler(s CollectionService, logger *zap.SugaredLogger, apikey *utils.APIKeyRepository) *CollectionHandler {
+	return &CollectionHandler{
+		service: s,
+		validator: validator.New(),
+		logger:  logger,
+		apikey:  apikey,
 	}
 }
 
-func (c *HTTPClient) CallSOAP(ctx context.Context, certName, endpoint, soapAction string, requestBody interface{}, responseBody interface{}) error {
-	xmlBytes, err := xml.Marshal(requestBody)
+// RegisterRoutes registers all routes related to Collection to the router group
+func (h *CollectionHandler) RegisterRoutes(rg *gin.RouterGroup) {
+	collectionRoutes := rg.Group("/Collection")
+	{
+		collectionRoutes.POST("/CollectionDetail", h.CollectionDetail)
+		collectionRoutes.POST("/CollectionLog", h.CollectionLog)
+	}
+}
+
+// CollectionDetail
+// @Tags         Common
+// @Accept       json
+// @Produce      json
+// @Param        Api-Key              header    string                               false  "API key"
+// @Param        request              body      domain.CollectionDetailRequest       false  "Body Request"
+// @Success      200  {object}        domain.CollectionDetailResponse
+// @Router       /Api/Collection/CollectionDetail [post]
+func (h *CollectionHandler) CollectionDetail(c *gin.Context) {
+	var req domain.CollectionDetailRequest
+
+	err := ValidateHeadersAndAuth(c, c.Request.Method, c.FullPath(), h.apikey, h.logger)
 	if err != nil {
-		return fmt.Errorf("failed to marshal SOAP request: %w", err)
+		handleErrorResponse(c, err)
+		return
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewReader(xmlBytes))
+	if err := c.ShouldBindJSON(&req); err != nil {
+		handleErrorResponse(c, app_error.ErrService)
+		return
+	}
+
+	if err := h.validator.Struct(req); err != nil {
+    	handleErrorResponse(c, HandleValidationError(err))
+    	return
+	}
+
+	response, appErr := h.service.CollectionDetail(c, req)
+	if appErr != nil {
+		handleErrorResponse(c, appErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, response)
+}
+
+// CollectionDetail
+// @Tags         Common
+// @Accept       json
+// @Produce      json
+// @Param        Api-Key              header    string                               false  "API key"
+// @Param        request              body      domain.CollectionLogRequest       false  "Body Request"
+// @Success      200  {object}        domain.CollectionLogResponse
+// @Router       /Api/Collection/CollectionLog [post]
+func (h *CollectionHandler) CollectionLog(c *gin.Context) {
+	var req domain.CollectionLogRequest
+
+	err := ValidateHeadersAndAuth(c, c.Request.Method, c.FullPath(), h.apikey, h.logger)
 	if err != nil {
-		return fmt.Errorf("failed to create HTTP request: %w", err)
+		handleErrorResponse(c, err)
+		return
 	}
 
-	req.Header.Set("Content-Type", "text/xml; charset=utf-8")
-	req.Header.Set("SOAPAction", soapAction)
-
-	c.logger.Infow("Sending SOAP request", "url", endpoint, "soapAction", soapAction, "requestBody", string(xmlBytes))
-
-	resp, err := c.DoWithCert(certName, endpoint, req)
-	if err != nil {
-		return fmt.Errorf("failed to send SOAP request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("failed to read SOAP response: %w", err)
+	if err := c.ShouldBindJSON(&req); err != nil {
+		handleErrorResponse(c, app_error.ErrInternalServer)
+		return
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("SOAP request failed status=%d body=%s", resp.StatusCode, string(bodyBytes))
+	if err := h.validator.Struct(req); err != nil {
+    	handleErrorResponse(c, HandleValidationError(err))
+    	return
+	}
+	
+	response, appErr := h.service.CollectionLog(c, req)
+	if appErr != nil {
+		handleErrorResponse(c, appErr)
+		return
 	}
 
-	c.logger.Infow("Received SOAP response", "status", resp.StatusCode, "responseBody", string(bodyBytes))
-
-	if err := xml.Unmarshal(bodyBytes, responseBody); err != nil {
-		return fmt.Errorf("failed to unmarshal SOAP response: %w", err)
-	}
-	return nil
+	c.JSON(http.StatusOK, response)
 }
