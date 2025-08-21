43dafd7c2b51bc77cfcdd1849516e94cc4a3f0f0
POng-haha <uroboros54150@gmail.com>
2025-08-21 20:43:20 +0700

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,3 @@
-// @title           Template Go API
-// @version         1.0
-// @description     This is a sample API documentation for our Go API application.
-// @termsOfService  http://swagger.io/terms/
-
-// @contact.name   SYE
-// @contact.url    https://aeon.co.th
-// @contact.email  sye@aeon.co.th
-
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host            localhost:8080
-// @BasePath        /api
-
 package main
 
 import (
@@ -22,20 +7,45 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	tcp_client_adapter "connectorapi-go/internal/core/client"
+	tcp_client_adapter "connectorapi-go/internal/adapter/client"
 	handler_adapter "connectorapi-go/internal/adapter/handler/api"
-	repo_adapter "connectorapi-go/internal/adapter/repository"
+	repo_adapter "connectorapi-go/internal/adapter/utils"
 	service_core "connectorapi-go/internal/core/service"
 	"connectorapi-go/pkg/config"
 	"connectorapi-go/pkg/logger"
 	"connectorapi-go/pkg/metrics"
 )
 
-// main is the primary function that starts the API Gateway
+// @title           Connector API Gateway
+// @version         1.0
+// @description     This is the API Gateway for ConnectorAPI.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   SYE
+// @contact.url    https://aeon.co.th
+// @contact.email  sye@aeon.co.th
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      10.254.97.103:8082
+// @BasePath  /
+
+// @schemes http https
 func main() {
 	cfg, err := config.Load("./configs/config.yaml")
 	if err != nil {
 		log.Fatalf("FATAL: Failed to load configuration: %v", err)
+	}
+
+	apiKeys, err := config.LoadAPIKeys("./configs/apikeys.json")
+	if err != nil {
+		log.Fatalf("FATAL: Failed to load apiKeys: %v", err)
+	}
+
+	dr, err := config.LoadDestinationsAndRoutes("./configs/destinations_routes.json")
+	if err != nil {
+		log.Fatalf("FATAL: Failed to load destinations and routes: %v", err)
 	}
 
 	appLogger := logger.New(cfg.Logger.Level)
@@ -49,31 +59,27 @@ func main() {
 	metrics.Init()
 
 	// --- Adapters ---
-	apiKeyRepo := repo_adapter.NewAPIKeyRepository(cfg)
+	apiKeyRepo := repo_adapter.NewAPIKeyRepository(apiKeys)
 
 	// --- TCP Socket Client Initialization ---
-	// Instantiate the BasicTCPSocketClient with appropriate timeouts.
-	// You might want to make these timeouts configurable via config.yaml.
 	tcpClient := tcp_client_adapter.NewBasicTCPSocketClient(
 		5*time.Second,  // Dial Timeout (e.g., 5 seconds to establish connection)
 		10*time.Second, // Read/Write Timeout (e.g., 10 seconds for data transfer)
 	)
 	appLogger.Info("TCP Socket Client initialized")
+	
+	appLogger.Infow("Loaded routes", "routes", dr.Routes)
+	appLogger.Infow("Loaded destinations", "destinations", dr.Destinations)
 
 	// --- Core Services ---
-	// Each service handles a specific domain of business logic
-	// Now, inject the new tcpClient into CustomerService.
-	customerService := service_core.NewCustomerService(cfg, appLogger, tcpClient)
-	collectionService := service_core.NewCollectionrService(cfg, appLogger, tcpClient)
+	collectionService := service_core.NewCollectionService(cfg, appLogger, tcpClient, dr.Routes, dr.Destinations)
 	appLogger.Info("Customer Service initialized with TCP client")
 
 	// --- Handlers (API Layer) ---
-	// Handlers are the entry point for API requests and depend on services
-	customerHandler := handler_adapter.NewCustomerHandler(customerService)
-	collectionHandler := handler_adapter.NewCollectionHandler(collectionService)
+	collectionHandler := handler_adapter.NewCollectionHandler(collectionService, appLogger, apiKeyRepo)
 
 	appLogger.Info("Setting up router...")
-	router := handler_adapter.SetupRouter(appLogger, apiKeyRepo, customerHandler, collectionHandler)
+	router := handler_adapter.SetupRouter(appLogger, apiKeyRepo, collectionHandler)
 
 	serverAddress := fmt.Sprintf(":%s", cfg.Server.Port)
 	appLogger.Infow("Starting server", "address", serverAddress)
