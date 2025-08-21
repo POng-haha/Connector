78675de9e6de6d4bca5cf15e374ebc6fefd041c5
POng-haha <uroboros54150@gmail.com>
2025-08-21 20:45:54 +0700

    Update collection_handler.go

--- internal/adapter/handler/api/collection_handler.go
+++ internal/adapter/handler/api/collection_handler.go
@@ -1,134 +1,119 @@
-package client
+package handler
 
 import (
+	"net/http"
+	
 	"connectorapi-go/internal/adapter/utils"
-	"fmt"             
-	"io"
-	"net"  // For TCP connections
-	"time" // For timeouts
+	"connectorapi-go/internal/core/domain"
+	app_error "connectorapi-go/pkg/error"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+
+	"go.uber.org/zap"
 )
 
-// TCPSocketClient defines the interface for a TCP socket client.
-type TCPSocketClient interface {
-	SendAndReceive(address string, combinedPayloadString string) (string, error)
+// CollectionService defines the interface
+type CollectionService interface {
+	CollectionDetail(c *gin.Context, reqData domain.CollectionDetailRequest) (*domain.CollectionDetailResponse, *app_error.AppError)
+	CollectionLog(c *gin.Context, reqData domain.CollectionLogRequest) (*domain.CollectionLogResponse, *app_error.AppError)
 }
 
-// BasicTCPSocketClient implements TCPSocketClient with length-prefixing and TIS-620 encoding.
-type BasicTCPSocketClient struct {
-	DialTimeout      time.Duration // Timeout for establishing the connection
-	ReadWriteTimeout time.Duration // Timeout for read/write operations
+// CollectionHandler handles all Collection-related API requests
+type CollectionHandler struct {
+	service CollectionService
+	validator *validator.Validate
+	logger  *zap.SugaredLogger
+	apikey  *utils.APIKeyRepository
 }
 
-// NewBasicTCPSocketClient creates a new instance of BasicTCPSocketClient.
-func NewBasicTCPSocketClient(dialTimeout, readWriteTimeout time.Duration) *BasicTCPSocketClient {
-	return &BasicTCPSocketClient{
-		DialTimeout:      dialTimeout,
-		ReadWriteTimeout: readWriteTimeout,
+// NewCollectionHandler creates a new instance of CollectionHandler
+func NewCollectionHandler(s CollectionService, logger *zap.SugaredLogger, apikey *utils.APIKeyRepository) *CollectionHandler {
+	return &CollectionHandler{
+		service: s,
+		validator: validator.New(),
+		logger:  logger,
+		apikey:  apikey,
 	}
 }
 
-// SendAndReceive connects to a TCP server, sends length-prefixed data, and receives a response.
-/*func (c *BasicTCPSocketClient) SendAndReceive(address string, requestPayloadUTF8 string) (string, error) {
+// RegisterRoutes registers all routes related to Collection to the router group
+func (h *CollectionHandler) RegisterRoutes(rg *gin.RouterGroup) {
+	collectionRoutes := rg.Group("/Collection")
+	{
+		collectionRoutes.POST("/CollectionDetail", h.CollectionDetail)
+		collectionRoutes.POST("/CollectionLog", h.CollectionLog)
+	}
+}
+
+// CollectionDetail
+// @Tags         Common
+// @Accept       json
+// @Produce      json
+// @Param        Api-Key              header    string                               false  "API key"
+// @Param        request              body      domain.CollectionDetailRequest       false  "Body Request"
+// @Success      200  {object}        domain.CollectionDetailResponse
+// @Router       /Api/Collection/CollectionDetail [post]
+func (h *CollectionHandler) CollectionDetail(c *gin.Context) {
+	var req domain.CollectionDetailRequest
+
+	err := ValidateHeadersAndAuth(c, c.Request.Method, c.FullPath(), h.apikey, h.logger)
+	if err != nil {
+		handleErrorResponse(c, err)
+		return
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		handleErrorResponse(c, app_error.ErrService)
+		return
+	}
+
+	if err := h.validator.Struct(req); err != nil {
+    	handleErrorResponse(c, HandleValidationError(err))
+    	return
+	}
+
+	response, appErr := h.service.CollectionDetail(c, req)
+	if appErr != nil {
+		handleErrorResponse(c, appErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, response)
+}
+
+// CollectionDetail
+// @Tags         Common
+// @Accept       json
+// @Produce      json
+// @Param        Api-Key              header    string                               false  "API key"
+// @Param        request              body      domain.CollectionLogRequest       false  "Body Request"
+// @Success      200  {object}        domain.CollectionLogResponse
+// @Router       /Api/Collection/CollectionLog [post]
+func (h *CollectionHandler) CollectionLog(c *gin.Context) {
+	var req domain.CollectionLogRequest
+
+	err := ValidateHeadersAndAuth(c, c.Request.Method, c.FullPath(), h.apikey, h.logger)
+	if err != nil {
+		handleErrorResponse(c, err)
+		return
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		handleErrorResponse(c, app_error.ErrInternalServer)
+		return
+	}
+
+	if err := h.validator.Struct(req); err != nil {
+    	handleErrorResponse(c, HandleValidationError(err))
+    	return
+	}
 	
-	conn, err := net.DialTimeout("tcp", address, c.DialTimeout)
-	if err != nil {
-		return "", fmt.Errorf("failed to dial TCP address %s: %w", address, err)
-	}
-	defer conn.Close()
-
-	requestPayload874, err := utils.Utf8ToCP874(requestPayloadUTF8)
-	if err != nil {
-		return "", fmt.Errorf("failed to encode request to CP874: %w", err)
+	response, appErr := h.service.CollectionLog(c, req)
+	if appErr != nil {
+		handleErrorResponse(c, appErr)
+		return
 	}
 
-	fmt.Printf("Sending TCP request payload 874 : % X\n", requestPayload874)
-
-	//length := uint32(len(requestPayload874))
-	//lengthBytes := make([]byte, 4)
-	//binary.BigEndian.PutUint32(lengthBytes, length)
-
-	//encodedRequest := append(lengthBytes, requestPayload874...)
-
-	//if err := conn.SetWriteDeadline(time.Now().Add(c.ReadWriteTimeout)); err != nil {
-	//	return "", fmt.Errorf("failed to set write deadline: %w", err)
-	//}
-
-	_, err = conn.Write(requestPayload874)
-	if err != nil {
-		return "", fmt.Errorf("failed to write data to TCP connection: %w", err)
-	}
-
-	//if err := conn.SetReadDeadline(time.Now().Add(c.ReadWriteTimeout)); err != nil {
-	//	return "", fmt.Errorf("failed to set read deadline: %w", err)
-	//}
-
-	responseBytes := make([]byte, 4096)
-	n, err := conn.Read(responseBytes)
-	if err != nil {
-		if err == io.EOF {
-			return "", fmt.Errorf("connection closed unexpectedly: %w", err)
-		}
-		return "", fmt.Errorf("connection closed unexpectedly: %w", err)
-	}
-	responseBytes = responseBytes[:n]
-
-	// Decode from CP874 to UTF-8
-	decoded, err := utils.DecodeCP874(responseBytes)
-	if err != nil {
-		return "", fmt.Errorf("failed to set read deadline: %w", err)
-	}
-
-	fmt.Printf("Receiving TCP reponse payload UTF8 : % X\n", decoded)
-
-	return decoded, nil
-}*/
-
-func (c *BasicTCPSocketClient) SendAndReceive(address string, combinedPayloadString string) (string, error) {
-
-	//fmt.Println("Param -> requestPayloadUTF8 :",combinedPayloadString)
-
-	// requestMsg := "AEON_WF   INQ_CUST_COSINF001RQ2025081820064204782025081801064200021                                                        4090610164364407DMEDฉันลองยิงเอพีไอมาน่ะ                                                                                                    ฉันลองยิงเอพีไอมาน่ะ2                                                                                                   ฉันลองยิงเอพีไอมาน่ะ3                                                                                                   ฉันลองยิงเอพีไอมาน่ะ4                                                                                                   ฉันลองยิงเอพีไอมาน่ะ5                                                                                                   20250820122600GSYS1234567891"
-
-	fmt.Println("Connecting to the server...")
-	conn, err := net.DialTimeout("tcp", address, c.DialTimeout)
-	if err != nil {
-		return "", fmt.Errorf("ER040: " + err.Error())
-	}
-	defer conn.Close()
-
-	conn.SetDeadline(time.Now().Add(c.DialTimeout))
-
-	fmt.Println("Encoding request to CP874...")
-	encodedRequest, err := utils.Utf8ToCP874(combinedPayloadString)
-	if err != nil {
-		return "", fmt.Errorf("ER040: Failed to encode request to CP874: " + err.Error())
-	}
-
-	//fmt.Printf("Sending bytes: % X\n", encodedRequest)
-	_, err = conn.Write(encodedRequest)
-	if err != nil {
-		return "", fmt.Errorf("ER060: " + err.Error())
-	}
-
-	fmt.Println("Request sent (UTF-8):", combinedPayloadString)
-
-	fmt.Println("Reading response...")
-	responseBytes := make([]byte, 4096)
-	n, err := conn.Read(responseBytes)
-	if err != nil {
-		if err == io.EOF {
-			return "", fmt.Errorf("ER060: Connection closed unexpectedly")
-		}
-		return "", fmt.Errorf("ER060: " + err.Error())
-	}
-	responseBytes = responseBytes[:n]
-
-	// Decode from CP874 to UTF-8
-	decoded, err := utils.DecodeCP874(responseBytes)
-	if err != nil {
-		return "", fmt.Errorf("ER040: Failed to decode response from CP874: " + err.Error())
-	}
-
-	fmt.Println("Final result (UTF-8):", decoded)
-	return decoded, nil
+	c.JSON(http.StatusOK, response)
 }
