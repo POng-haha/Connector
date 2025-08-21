24846a8cf5291fb66f1032720b62599f45e5d007
POng-haha <uroboros54150@gmail.com>
2025-08-21 20:54:16 +0700

    Update collection_service.go

--- internal/core/service/collection_service.go
+++ internal/core/service/collection_service.go
@@ -1,135 +1,227 @@
 package service
 
 import (
-	"encoding/json"
-	"connectorapi-go/internal/core/client"
+	"connectorapi-go/internal/adapter/client"
+	"connectorapi-go/internal/adapter/utils"
 	"connectorapi-go/internal/core/domain"
+	"connectorapi-go/internal/core/service/format"
 	"connectorapi-go/pkg/config"
-	"connectorapi-go/pkg/format"
+	"fmt"
+	"strings"
 
+	app_error "connectorapi-go/pkg/error"
+
+	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
+// TCPSocketClient defines the interface for a TCP socket client
+type collectionTCPSocketClient = client.TCPSocketClient
+
 // CollectionService implements the business logic for customer-related features
 type CollectionService struct {
-	config    *config.Config
-	logger    *zap.SugaredLogger
-	tcpClient *client.TCPSocketClient
+	config       *config.Config
+	logger       *zap.SugaredLogger
+	tcpClient    client.TCPSocketClient
+	routes       map[string]config.Route
+	destinations map[string]config.Destination
 }
 
 // NewCollectionService creates a new instance of CustomerService.
-func NewCollectionService(cfg *config.Config, logger *zap.SugaredLogger, tcpClient *client.TCPSocketClient) *CollectionService {
+func NewCollectionService(
+	cfg *config.Config,
+	logger *zap.SugaredLogger,
+	tcpClient collectionTCPSocketClient,
+	routes map[string]config.Route,
+	destinations map[string]config.Destination,
+) *CollectionService {
 	return &CollectionService{
-		config:    cfg,
-		logger:    logger,
-		tcpClient: tcpClient,
+		config:       cfg,
+		logger:       logger,
+		tcpClient:    tcpClient,
+		routes:       routes,
+		destinations: destinations,
 	}
 }
 
-// CollectionDetail handles the logic for retrieving customer information via TCP.
-func (s *CollectionService) CollectionDetail(reqData domain.CollectionDetailRequest, requestID string) (*domain.CollectionDetailResponse, *domain.AppError) {
-	const routeKey = "POST:/Api/Collection/CollectionDetail"
-	const destinationName = "systemi"
+// CollectionDetail handles the CollectionDetail request
+// It sends a request to the TCP service and returns the response.
+func (s *CollectionService) CollectionDetail(c *gin.Context, reqData domain.CollectionDetailRequest) (*domain.CollectionDetailResponse, *app_error.AppError) {
+	//const routeKey = "POST:/Api/Collection/CollectionDetail"
+	routeKey := utils.GetRouteKey(c)
+	const destinationName = "systemI"
+	//var logLine string
 
-	route, ok := s.config.Routes[routeKey]
+	idValue, _ := c.Get("Api-RequestID")
+	apiRequestID, ok := idValue.(string)
 	if !ok {
-		s.logger.Errorw("Route configuration not found for TCP service", "routeKey", routeKey)
-		return nil, domain.ErrConfig
+		apiRequestID = ""
 	}
 
-	destination, ok := s.config.Destinations[destinationName]
+	route, ok := s.routes[routeKey]
+	if !ok {
+		s.logger.Errorw("Route configuration not found for TCP service", "routeKey", routeKey)
+		return nil, app_error.ErrConfig
+	}
+
+	destination, ok := s.destinations[destinationName]
 	if !ok {
 		s.logger.Errorw("TCP Destination configuration not found", "destinationName", destinationName)
-		return nil, domain.ErrConfig
+		return nil, app_error.ErrConfig
 	}
 	if destination.Type != "tcp" {
 		s.logger.Errorw("Destination type is not TCP", "destinationName", destinationName, "type", destination.Type)
-		return nil, domain.ErrConfig
+		return nil, app_error.ErrConfig
 	}
-	tcpAddress := destination.Host
 
-	formattedRequestID := format.PadOrTruncate(requestID, 20)
-	fixedLengthData := format.FormatCollectionDetailRequestFixedLength(reqData)
+	portList, ok := destination.Ports["CollectionDetail"]
+	if !ok || len(portList) == 0 {
+		s.logger.Errorw("Invalid port configuration", "port", portList)
+		return nil, app_error.ErrConfig
+	}
+	port := utils.RandomPortFromList(portList)
+	if port == "" {
+		s.logger.Errorw("Invalid port configuration", "port", portList)
+		return nil, app_error.ErrConfig
+	}
 
-	header := format.BuildFixedLengthHeader(
+	tcpAddress := fmt.Sprintf("%s:%s", destination.IP, port)
+
+	formattedRequestID := utils.PadOrTruncate(apiRequestID, 20)
+	fixedLengthData := format.FormatCollectionDetailRequest(reqData)
+
+	header := utils.BuildFixedLengthHeader(
 		route.System,
 		route.Service,
 		route.Format,
 		formattedRequestID,
-		fixedLengthData,
+		route.RequestLength,
 	)
 
 	combinedPayloadString := header + fixedLengthData
-	requestPayload := []byte(combinedPayloadString)
 
-	s.logger.Debugw("Sending TCP request payload", "payload", combinedPayloadString)
+	s.logger.Info("Sending TCP request payload : ", combinedPayloadString)
 
-	responseBytes, err := s.tcpClient.SendAndReceive(tcpAddress, requestPayload)
+	responseStr, err := s.tcpClient.SendAndReceive(tcpAddress, combinedPayloadString)
 	if err != nil {
 		s.logger.Errorw("Downstream TCP service call failed", "error", err, "address", tcpAddress)
-		return nil, domain.ErrServiceDown
+		return nil, app_error.ErrService
 	}
 
-	s.logger.Debugw("Received downstream TCP response", "response", string(responseBytes))
+	// Check if the response contains an error
+	errorCode := strings.TrimSpace(responseStr[67:73])
+	errorMessage := strings.TrimSpace(responseStr[73:123])
 
-	var CollectionDetailResponse domain.CollectionDetailResponse
-	if err := json.Unmarshal(responseBytes, &CollectionDetailResponse); err != nil {
-		s.logger.Errorw("Failed to decode downstream TCP response", "error", err)
-		return nil, domain.ErrInternalServer
+	switch errorCode {
+	case "SVC105":
+		return nil, app_error.ErrRequiedParam
+	case "SVC117":
+		return nil, app_error.ErrIDCardNotFound
+	case "SVC902":
+		return nil, app_error.ErrSystemI
+	case "SVC203":
+		return nil, app_error.ErrSUEInfoNotFound
+	default:
+    	if errorCode != "" {
+        	s.logger.Info("Unknown error code from System I : ",
+         	   "code", errorCode,
+          	  "message", errorMessage,
+        	)
+        	return nil, app_error.ErrSystemIUnexpect
+    	}
 	}
-	return &CollectionDetailResponse, nil
+
+	s.logger.Info("Received downstream TCP response", "response", string(responseStr))
+
+	response, _ := format.FormatCollectionDetailResponse(responseStr)
+	return &response, nil
 }
 
-// CollectionLog handles the logic for updating a customer's address via TCP.
-func (s *CollectionService) CollectionLog(reqData domain.CollectionLogRequest, requestID string) (*domain.CollectionLogResponse, *domain.AppError) {
-	const routeKey = "POST:/Api/Collection/CollectionLog"
-	const destinationName = "systemi"
+// CollectionLog handles the logic for logging collection data via TCP.
+// It sends a request to the TCP service and returns the response.
+func (s *CollectionService) CollectionLog(c *gin.Context, reqData domain.CollectionLogRequest) (*domain.CollectionLogResponse, *app_error.AppError) {
+	//const routeKey = "POST:/Api/Collection/CollectionLog"
+	routeKey := utils.GetRouteKey(c)
+	const destinationName = "systemI"
 
-	route, ok := s.config.Routes[routeKey]
+	idValue, _ := c.Get("Api-RequestID")
+	apiRequestID, ok := idValue.(string)
 	if !ok {
-		s.logger.Errorw("Route configuration not found for TCP service", "routeKey", routeKey)
-		return nil, domain.ErrConfig
+		apiRequestID = ""
 	}
 
-	destination, ok := s.config.Destinations[destinationName]
+	route, ok := s.routes[routeKey]
+	if !ok {
+		s.logger.Errorw("Route configuration not found for TCP service", "routeKey", routeKey)
+		return nil, app_error.ErrConfig
+	}
+
+	destination, ok := s.destinations[destinationName]
 	if !ok {
 		s.logger.Errorw("TCP Destination configuration not found", "destinationName", destinationName)
-		return nil, domain.ErrConfig
+		return nil, app_error.ErrConfig
 	}
 	if destination.Type != "tcp" {
 		s.logger.Errorw("Destination type is not TCP", "destinationName", destinationName, "type", destination.Type)
-		return nil, domain.ErrConfig
+		return nil, app_error.ErrConfig
 	}
-	tcpAddress := destination.Host
 
-	formattedRequestID := format.PadOrTruncate(requestID, 20)
-	fixedLengthData := format.FormatCollectionLogRequestFixedLength(reqData)
+	portList, ok := destination.Ports["CollectionDetail"]
+	if !ok || len(portList) == 0 {
+		s.logger.Errorw("Invalid port configuration", "port", portList)
+		return nil, app_error.ErrConfig
+	}
+	port := utils.RandomPortFromList(portList)
+	if port == "" {
+		s.logger.Errorw("Invalid port configuration", "port", portList)
+		return nil, app_error.ErrConfig
+	}
 
-	header := format.BuildFixedLengthHeader(
+	tcpAddress := fmt.Sprintf("%s:%s", destination.IP, port)
+
+	formattedRequestID := utils.PadOrTruncate(apiRequestID, 20)
+	fixedLengthData := format.FormatCollectionLogRequest(reqData)
+
+	header := utils.BuildFixedLengthHeader(
 		route.System,
 		route.Service,
 		route.Format,
 		formattedRequestID,
-		fixedLengthData,
+		route.RequestLength,
 	)
 
 	combinedPayloadString := header + fixedLengthData
-	requestPayload := []byte(combinedPayloadString)
+	s.logger.Info("Sending TCP request payload", "payload", combinedPayloadString)
 
-	s.logger.Debugw("Sending TCP request payload", "payload", combinedPayloadString)
-
-	responseBytes, err := s.tcpClient.SendAndReceive(tcpAddress, requestPayload)
+	responseStr, err := s.tcpClient.SendAndReceive(tcpAddress, combinedPayloadString)
 	if err != nil {
 		s.logger.Errorw("Downstream TCP service call failed", "error", err, "address", tcpAddress)
-		return nil, domain.ErrServiceDown
+		return nil, app_error.ErrService
 	}
 
-	s.logger.Debugw("Received downstream TCP response", "response", string(responseBytes))
+	// Check if the response contains an error
+	errorCode := strings.TrimSpace(responseStr[67:73])
+	errorMessage := strings.TrimSpace(responseStr[73:123])
 
-	var CollectionLogResponse domain.CollectionLogResponse
-	if err := json.Unmarshal(responseBytes, &CollectionLogResponse); err != nil {
-		s.logger.Errorw("Failed to decode downstream TCP response", "error", err)
-		return nil, domain.ErrInternalServer
+	switch errorCode {
+	case "SVC216", "SVC235", "SVC342", "SVC343", "SCV344":
+		return nil, app_error.ErrRequiedParam
+	case "SVC236":
+		return nil, app_error.ErrAgmNoNotFound
+	case "SVC902":
+		return nil, app_error.ErrSystemI
+	default:
+    	if errorCode != "" {
+        	s.logger.Info("Unknown error code from System I : ",
+         	   "code", errorCode,
+          	  "message", errorMessage,
+        	)
+        	return nil, app_error.ErrSystemIUnexpect
+    	}
 	}
-	return &CollectionLogResponse, nil
+
+	s.logger.Debugw("Received downstream TCP response", "response", string(responseStr))
+
+	response, _:= format.FormatCollectionLogResponse(responseStr)
+	return &response, nil
 }
